144c432c06184595c9b90684c83e1e8db2d60dbb
BrianTirafiA <114814776+BrianTirafiA@users.noreply.github.com>
2025-08-26 06:13:36 +0700

    Making UI responsive

---
+++ My project/Assets/Script/OnboardingManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnboardingManager : MonoBehaviour
+{
+    public GameObject[] pages;
+    public Button selanjutnyaButton;
+    public Button mulaiSekarangButton;
+    //public Animator dotAnimator;
+    private int currentPageIndex = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i].SetActive(false);
+        }
+
+        if (pages.Length > 0)
+        {
+            pages[0].SetActive(true);
+        }
+
+        selanjutnyaButton.gameObject.SetActive(true);
+        mulaiSekarangButton.gameObject.SetActive(false);
+        selanjutnyaButton.onClick.AddListener(GoToNextPage);
+        //dotAnimator.SetInteger("PageIndex", 0);
+    }
+
+    public void GoToNextPage()
+    {
+        pages[currentPageIndex].SetActive(false);
+        currentPageIndex++;
+
+        if (currentPageIndex < pages.Length)
+        {
+            pages[currentPageIndex].SetActive(true);
+            //dotAnimator.SetInteger("PageIndex", 1);
+        }
+
+        if (currentPageIndex == pages.Length - 1)
+        {
+            selanjutnyaButton.gameObject.SetActive(false);
+            mulaiSekarangButton.gameObject.SetActive(true);
+        }
+    }
+}
