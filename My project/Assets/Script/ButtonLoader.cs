50d46a5bdc71ec513d889139dae6f3da0ac88f07
BrianTirafiA <114814776+BrianTirafiA@users.noreply.github.com>
2025-08-28 16:34:32 +0700

    Adding Animation

--- My project/Assets/Script/ButtonLoader.cs
+++ My project/Assets/Script/ButtonLoader.cs
@@ -1,16 +1,30 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using static System.TimeZoneInfo;
 
 public class ButtonLoader : MonoBehaviour
 {
     public string Load1;
     public string Load2;
+    public Animator pageAnimator;
+    public float transitionTime = 2f;
+    private string sceneToLoad;
     public void Load1Scene()
     {
-        SceneManager.LoadScene(Load1);
+        sceneToLoad = Load1;
+        StartCoroutine(LoadSceneAfterAnimation());
     }
     public void Load2Scene()
     {
-        SceneManager.LoadScene(Load2);
+        sceneToLoad = Load2;
+        StartCoroutine(LoadSceneAfterAnimation());
+    }
+    IEnumerator LoadSceneAfterAnimation()
+    {
+        pageAnimator.SetTrigger("Next");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
